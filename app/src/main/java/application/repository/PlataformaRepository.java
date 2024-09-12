ec7ea706b9fb4f89a6be271677c003c6e5f2868a
Tamaracaroline08 <tamaracmarcelino@gmail.com>
2024-09-12 23:43:22 +0000

    Salvar

--- app/src/main/java/application/repository/PlataformaRepository.java
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -1,1 +1,11 @@
 package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Plataforma;
+
+public interface PlataformaRepository extends CrudRepository<Plataforma, Long> {
+    
+}
+
+
